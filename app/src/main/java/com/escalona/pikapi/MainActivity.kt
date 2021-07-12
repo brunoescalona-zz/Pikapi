6ab3293171183ada01b196e31758a769abecb656
Bruno Escalona <escalona.ruiz@stocard.de>
2021-07-12 22:06:41 +0200

    Display the pokemon list in the UI

--- app/src/main/java/com/escalona/pikapi/MainActivity.kt
+++ app/src/main/java/com/escalona/pikapi/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.escalona.pikapi
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.escalona.pikapi.data.PokeApiRepository
 import com.escalona.pikapi.ui.theme.PikapiTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,33 +17,18 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var repository: PokeApiRepository
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PikapiTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PokemonListView(viewModel)
                 }
             }
         }
-
-        val viewModel: MainViewModel by viewModels()
-        viewModel.pokemonList.observe(this, { pokemonList ->
-            Log.d("List of pokemons", "$pokemonList")
-        })
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PikapiTheme {
-        Greeting("Android")
-    }
-}
